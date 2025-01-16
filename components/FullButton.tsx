e9082057f450d32e729abebd48db55acc8f1abe9
fabiconcept <favourajokubi@gmail.com>
2025-01-16 15:38:41 +0100

    Animate FullButton component

--- components/FullButton.tsx
+++ components/FullButton.tsx
@@ -1,23 +1,44 @@
 import { Text, TouchableOpacity } from 'react-native';
 import clsx from 'clsx';
 import { ButtonProps } from '@/types/button';
+import Animated, { useAnimatedStyle, withSpring } from 'react-native-reanimated';
+import { useState } from 'react';
 
 export default function FullButton(props: ButtonProps) {
     const {
         iconLeft: IconLeft,
         iconRight: IconRight
-    } = props
+    } = props;
+    const [isPressed, setIsPressed] = useState(false);
+
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    scale: withSpring(isPressed ? 0.95 : 1, {
+                        damping: 10,
+                        stiffness: 200
+                    }),
+                },
+            ],
+        };
+    });
+
     return (
-        <TouchableOpacity
-            className={clsx(
-                "py-4 rounded-xl flex-row items-center justify-center",
-                props.className
-            )}
-            {...props}
-        >
-            {IconLeft && <IconLeft />}
-            <Text className={clsx('text-center text-base', props.textClassName)}>{props.title}</Text>
-            {IconRight && <IconRight />}
-        </TouchableOpacity>
+        <Animated.View style={animatedStyle}>
+            <TouchableOpacity
+                className={clsx(
+                    "py-4 rounded-xl flex-row items-center justify-center",
+                    props.className
+                )}
+                onPressIn={() => setIsPressed(true)}
+                onPressOut={() => setIsPressed(false)}
+                {...props}
+            >
+                {IconLeft && <IconLeft />}
+                <Text className={clsx('text-center text-base', props.textClassName)}>{props.title}</Text>
+                {IconRight && <IconRight />}
+            </TouchableOpacity>
+        </Animated.View>
     )
 }
