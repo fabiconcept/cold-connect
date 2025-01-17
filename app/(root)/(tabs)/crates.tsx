304c0af51dcf7fdda728f1d061f89b13c6c9471f
fabiconcept <favourajokubi@gmail.com>
2025-01-17 19:15:55 +0100

    Design Crates Page

--- app/(root)/(tabs)/crates.tsx
+++ app/(root)/(tabs)/crates.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native';
+import CrateView from '@/components/Crates/CrateView';
+import Header from '@/components/Crates/Header';
+import Options from '@/components/Crates/Options';
+import { ScrollView, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Crates() {
     return (
-        <View className='flex-1 items-center justify-center'>
-            <Text className='text-4xl font-MontserratSemiBold text-primary'>Crates</Text>
-        </View>
+        <SafeAreaView className='flex-1'>
+            <ScrollView>
+                <StatusBar barStyle={"dark-content"} />
+                <Header />
+                <CrateView />
+                <Options />
+            </ScrollView>
+        </SafeAreaView>
     )
 }
