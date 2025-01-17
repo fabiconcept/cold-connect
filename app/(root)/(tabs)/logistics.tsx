48ebee0a0413b539cf69d7030eec39fd573fe96e
fabiconcept <favourajokubi@gmail.com>
2025-01-17 20:19:49 +0100

    Design Logistics page

--- app/(root)/(tabs)/logistics.tsx
+++ app/(root)/(tabs)/logistics.tsx
@@ -1,9 +1,32 @@
-import { View, Text } from 'react-native';
+import TruckSmall from '@/assets/svgs/logistics/truck-small';
+import Header from '@/components/Logistics/Header';
+import Options from '@/components/Logistics/Options';
+import { Image } from 'react-native';
+import { ScrollView, StatusBar, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Logistics() {
     return (
-        <View className='flex-1 items-center justify-center'>
-            <Text className='text-4xl font-MontserratSemiBold text-primary'>Logistics</Text>
-        </View>
+        <SafeAreaView className='flex-1'>
+            <StatusBar barStyle={"dark-content"} />
+            <ScrollView>
+                <Header />
+                <View className='mb-8 mt-10 justify-center items-center w-full'>
+                    <Image
+                        source={require("@/assets/images/cold/ellipse-24.png")}
+                        resizeMode='contain'
+                        className='absolute top-0 left-0 w-full opacity-50'
+                    />
+                    <TruckSmall
+                        width={'90%'}
+                        style={{
+                            marginLeft: 16
+                        }}
+                    />
+                </View>
+                <Options />
+                <View className='h-32'></View>
+            </ScrollView>
+        </SafeAreaView >
     )
 }
