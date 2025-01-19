7a15b92c425590ee3b793ee739d39fe30afc7c7e
fabiconcept <favourajokubi@gmail.com>
2025-01-19 13:27:17 +0100

    Update the header components for crates & logistics pages to have a back button

--- components/Crates/Header.tsx
+++ components/Crates/Header.tsx
@@ -1,9 +1,23 @@
-import { View, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default function Header() {
     return (
         <View className='px-3 z-20 absolute left-0 top-5 w-full flex-row justify-between items-center'>
-            <Text className='font-semibold text-xl text-center w-full font-MontserratSemiBold'>Buy Crates</Text>
+            <TouchableOpacity
+                hitSlop={20}
+                className='w-[50px] h-[50px] justify-center items-center rounded-full bg-white shadow-lg'
+                onPress={() => router.back()}
+            >
+                <Feather
+                    name="chevron-left"
+                    size={24}
+                    color="black"
+                />
+            </TouchableOpacity>
+            <Text className='flex-1 font-semibold text-xl text-center w-full font-MontserratSemiBold'>Buy Crates</Text>
+            <View className='w-[50px]' />
         </View>
     )
 }
--- components/Logistics/Header.tsx
+++ components/Logistics/Header.tsx
@@ -1,9 +1,23 @@
-import { View, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default function Header() {
     return (
         <View className='px-3 z-20 absolute left-0 top-5 w-full flex-row justify-between items-center'>
-            <Text className='font-semibold text-xl text-center w-full font-MontserratSemiBold'>Cold Logistics</Text>
+            <TouchableOpacity
+                hitSlop={20}
+                className='w-[50px] h-[50px] justify-center items-center rounded-full bg-white shadow-lg'
+                onPress={() => router.back()}
+            >
+                <Feather
+                    name="chevron-left"
+                    size={24}
+                    color="black"
+                />
+            </TouchableOpacity>
+            <Text className='flex-1font-semibold text-xl text-center w-full font-MontserratSemiBold'>Cold Logistics</Text>
+            <View className='w-[50px]' />
         </View>
     )
 }
