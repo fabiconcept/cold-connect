c51583faa25700e58e552d380bfb73efd3a8166f
fabiconcept <favourajokubi@gmail.com>
2025-01-21 22:12:51 +0100

    Add action sheet to cart page

--- app/(root)/(tabs)/crates.tsx
+++ app/(root)/(tabs)/crates.tsx
@@ -1,18 +1,25 @@
 import CrateView from '@/components/Crates/CrateView';
 import Header from '@/components/Crates/Header';
 import Options from '@/components/Crates/Options';
-import { ScrollView, StatusBar } from 'react-native';
+import ActionSheet from '@/components/general sub components/ActionSheet';
+import { ScrollView, StatusBar, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Crates() {
     return (
         <SafeAreaView className='flex-1'>
             <StatusBar barStyle={"dark-content"} />
-            <ScrollView>
-                <Header />
+            <Header />
+            <ScrollView className='flex-1'>
                 <CrateView />
                 <Options />
+                <View className='h-36' />
             </ScrollView>
+            <ActionSheet
+                action={() => { }}
+                actionText='Add to Cart'
+                value={2700}
+            />
         </SafeAreaView>
     )
 }
