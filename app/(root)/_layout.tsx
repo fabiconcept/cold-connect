b5bee0b7f30d82635fc7ecd7d41182192e3cad0a
fabiconcept <favourajokubi@gmail.com>
2025-01-21 08:26:18 +0100

    Fix <GoToCartButton /> placement

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -3,15 +3,17 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="enable-location" options={{ headerShown: false }} />
-            <Stack.Screen name="my-invoices" options={{ headerShown: false }} />
-            <Stack.Screen name="cold-storage/[id]" options={{ headerShown: false }} />
-            <Stack.Screen name="product-category/[id]" options={{ headerShown: false }} />
-            <Stack.Screen name="product-category" options={{ headerShown: false }} />
-            <Stack.Screen name="cart" options={{ headerShown: false }} />
+        <>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="enable-location" options={{ headerShown: false }} />
+                <Stack.Screen name="my-invoices" options={{ headerShown: false }} />
+                <Stack.Screen name="cold-storage/[id]" options={{ headerShown: false }} />
+                <Stack.Screen name="product-category/[id]" options={{ headerShown: false }} />
+                <Stack.Screen name="product-category" options={{ headerShown: false }} />
+                <Stack.Screen name="cart" options={{ headerShown: false }} />
+            </Stack>
             <GoToCartButton />
-        </Stack>
+        </>
     )
 }
