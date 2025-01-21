884067bb0ba42244e5ae0c521b39b4efa0bcd6ff
fabiconcept <favourajokubi@gmail.com>
2025-01-21 08:23:22 +0100

    Design Cold Cart Page

---
+++ app/(root)/cart.tsx
@@ -0,0 +1,28 @@
+import CartTray from '@/components/Cart/CartTray';
+import Header from '@/components/Cart/Header';
+import { Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+export default function cart() {
+    return (
+
+        <SafeAreaView className='flex-1'>
+            <Image
+                source={require("@/assets/images/cold/cart-ice-01.png")}
+
+                resizeMode='contain'
+
+                className='absolute top-0 right-0'
+            />
+            <Image
+                source={require("@/assets/images/cold/cart-ice-02.png")}
+                resizeMode='contain'
+                className='absolute bottom-0 left-0'
+            />
+            <Header />
+            <CartTray />
+        </SafeAreaView>
+    )
+
+}
