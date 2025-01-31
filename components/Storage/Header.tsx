769e78abfae11d63ffaf5b05e27c912c4348f955
fabiconcept <favourajokubi@gmail.com>
2025-01-31 23:09:15 +0100

    Implement Searching and use my region actions

--- components/Storage/Header.tsx
+++ components/Storage/Header.tsx
@@ -1,9 +1,41 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, TouchableOpacity, Platform, ToastAndroid } from 'react-native';
 import InputField from '../Form/InputField';
 import Target from '@/assets/svgs/storage/target';
+import { useHubsByRegion } from '@/store/Public/Public Storage Feed Endpoints/hubsByRegion';
+import { getGeoRegionInNigeria } from '@/lib/utilities';
+import { useLocationStore } from '@/store';
+import { useEffect, useState } from 'react';
+import { useDebounce } from '@/lib/Hooks/useDebounce';
 
 
 export default function Header() {
+    const { setSelectedRegion, load_hubs, search_for_hubs, reset_search, selectedRegion } = useHubsByRegion();
+    const { latitude, longitude } = useLocationStore();
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const debouncedSearchQuery = useDebounce(searchQuery, 500);
+
+    useEffect(() => {
+        if (debouncedSearchQuery) {
+            search_for_hubs(debouncedSearchQuery);
+        } else {
+            reset_search();
+            load_hubs(selectedRegion!);
+        }
+    }, [debouncedSearchQuery]);
+
+    const loadHubsAroundMe = async () => {
+        const region = getGeoRegionInNigeria(latitude!, longitude!);
+        const userRegion = region === "unknown" ? "" : region;
+
+        if (Platform.OS === 'android') {
+            ToastAndroid.show("Load Hubs Around Me", ToastAndroid.SHORT);
+        }
+
+        setSelectedRegion(userRegion);
+        load_hubs(userRegion);
+    }
+
     return (
         <View className='h-[117px] bg-primary flex-row items-end p-5 gap-3'>
             <View className='flex-1'>
@@ -15,8 +47,11 @@ export default function Header() {
                     containerStyle='rounded-full px-2'
                     iconStyle='opacity-60'
                     fullyRounded={true}
+                    value={searchQuery}
+                    onChangeText={setSearchQuery}
                 />
                 <TouchableOpacity
+                    onPress={loadHubsAroundMe}
                     className='bg-white h-[50px] w-[50px] items-center justify-center rounded-full absolute top-[35%] right-0 -translate-y-1/2'
                 >
                     <Target
