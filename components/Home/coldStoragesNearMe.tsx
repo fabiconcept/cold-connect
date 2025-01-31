fef2611f847f5ff92f62f6d420ee4df1b2a018ab
fabiconcept <favourajokubi@gmail.com>
2025-01-31 23:11:34 +0100

    Implement useHubsAroundMe in ColdStoragesNearMe component

--- components/Home/coldStoragesNearMe.tsx
+++ components/Home/coldStoragesNearMe.tsx
@@ -1,21 +1,47 @@
-import { Link } from 'expo-router';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Swiper from '../Swiper';
 import StorageCard from './sub components/StorageCard';
+import { useHubsNearMe } from '@/store/Public/Public Storage Feed Endpoints/nearByHubs';
+import LoadingStorageCard from './sub components/LoadingStorageCard';
 
 
 export default function ColdStoragesNearMe() {
+    const { loading, storages, total } = useHubsNearMe();
+
+    if (loading) return (
+        <View className='mx-3 mt-5'>
+            <LoadingStorageCard />
+        </View>
+    );
+
     return (
         <View className='mx-3 mt-5'>
             <View className='flex-row items-center justify-between mx-2 my-2'>
-                <Text className='text-xl font-semibold font-MontserratSemiBold'>Cold Storages Near Me <Text className='text-base text-gray-400 font-MontserratSemiBold'>(03)</Text></Text>
-                <Link href={"/(root)/(tabs)/storage"} className='text-primary text-lg font-MontserratSemiBold'>See all</Link>
+                <Text className='text-xl font-semibold font-MontserratSemiBold'>Cold Storages Near Me <Text className='text-base text-gray-400 font-MontserratSemiBold'>({total})</Text></Text>
             </View>
-
             <Swiper
-                data={[0, 1, 2]}
+                data={storages}
                 renderItem={({ item }) => (
-                    <StorageCard />
+                    <StorageCard
+                        hub_name={item.name}
+                        imageSource={item.photo}
+                        address={item.location}
+                        rating={item.star}
+                        status={item.availability as "active" | "inactive"}
+                        distance={item.distance}
+                        hub_id={item.id}
+                    />
+                )}
+                ListEmptyComponent={() => (
+                    <View className='flex-1 items-center justify-center pt-10'>
+                        <Image
+                            source={require('@/assets/images/cold/no-result.png')}
+                            resizeMode='contain'
+                            className='w-36 h-36'
+                            alt='No Storages Found Near You'
+                        />
+                        <Text className='font-MontserratSemiBold font-semibold text-primary'>No Storages Found Near You</Text>
+                    </View>
                 )}
             />
         </View>
