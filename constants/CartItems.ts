24d4e5b88603e8086d24650125492512cca69154
fabiconcept <favourajokubi@gmail.com>
2025-02-04 09:46:14 +0100

    Create Cart icon constants

---
+++ constants/CartItems.ts
@@ -0,0 +1,34 @@
+export const CartItems = [
+    {
+        title: "Fish",
+        image: require("@/assets/images/cold/fish-cart-tem.png")
+    },
+    {
+        title: "Beverage",
+        image: require("@/assets/images/cold/bev-cart-tem.png")
+    },
+    {
+        title: "Dairy",
+        image: require("@/assets/images/cold/milk-cart-tem.png")
+    },
+    {
+        title: "Fruits",
+        image: require("@/assets/images/cold/fruits-cart-tem.png")
+    },
+    {
+        title: "Vegetables",
+        image: require("@/assets/images/cold/veg-cart-tem.png")
+    },
+    {
+        title: "Meat",
+        image: require("@/assets/images/cold/meat-cart-tem.png")
+    },
+    {
+        title: "Crates",
+        image: require("@/assets/images/cold/crate-cart-tem.png")
+    },
+    {
+        title: "Logistics",
+        image: require("@/assets/images/cold/logistics-cart-tem.png")
+    }
+];
