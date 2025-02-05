e94f69a6a0b91ce090d88c3a530c341852e5fa68
fabiconcept <favourajokubi@gmail.com>
2025-02-05 16:08:05 +0100

    `Update InformationSection component to support editable items`

--- components/Profile/InformationSection.tsx
+++ components/Profile/InformationSection.tsx
@@ -1,16 +1,20 @@
-import { InformationItem, InformationActionItem } from '@/types/types';
+import { useAuthenticationStore } from '@/store/auth';
+import { InformationItem, InformationActionItem, InformationItemEditable } from '@/types/types';
 import { Feather } from '@expo/vector-icons';
 import clsx from 'clsx';
-import { View, Text, TouchableOpacity } from 'react-native';
-import AnimatedPressable from '../general sub components/AnimatedPress';
+import { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function InformationSection({
     data,
     title
 }: {
-    data: (InformationItem | InformationActionItem)[],
+    data: (InformationItem | InformationActionItem | InformationItemEditable)[],
     title: string
 }) {
+
+
     return (
         <View className='mt-5'>
             <Text className='text-lg font-semibold font-MontserratSemiBold'>{title}</Text>
@@ -19,17 +23,21 @@ export default function InformationSection({
                     if (item.type === "text") {
                         return (
                             <View key={index} className={clsx(
-                                'flex-row justify-between items-center p-5',
+                                'flex-row justify-between items-center px-5',
+                                item.editable && "py-3",
+                                !item.editable && "py-5",
                                 index !== data.length - 1 && 'border-b border-gray-300'
                             )}>
                                 <Text className='opacity-50'>{item.title}</Text>
                                 <View className='flex-row gap-2 items-center justify-end flex-1'>
-                                    <View className='flex-1 items-end'>
-                                        <Text numberOfLines={1} className='text-right'>{item.value}</Text>
-                                    </View>
                                     {item.editable ? (
-                                        <Feather name="edit-3" size={20} color="#0066e1" />
-                                    ) : null}
+                                        <EditableItem item={item} />
+                                    ) : (
+                                        <View className='flex-1 items-end'>
+                                            <Text numberOfLines={1} className='text-right'>{item.value}</Text>
+                                        </View>
+                                    )
+                                    }
                                 </View>
                             </View>
                         )
@@ -49,7 +57,56 @@ export default function InformationSection({
                         )
                     }
                 })}
+            </View >
+        </View >
+    )
+}
+
+const EditableItem = ({ item }: { item: InformationItemEditable }) => {
+    const [editableItem, setEditableItem] = useState<boolean>(false);
+    const [value, setValue] = useState<string>(item.value.toLowerCase() === "n/a" ? "" : item.value);
+
+    const { updatingUser, updateProfile, activeId } = useAuthenticationStore();
+
+    const handleUpdate = async () => {
+        setEditableItem(false);
+        if (!value) return;
+
+        await updateProfile({
+            [item.name]: value.trim()
+        }, activeId);
+    }
+
+    return (
+        <>
+            <View className='flex-1 items-end'>
+                <TextInput
+                    className='h-10 py-0 -mb-1'
+                    placeholder={item.placeHolder}
+                    value={!editableItem ? item.value : value}
+                    onBlur={handleUpdate}
+                    onPointerLeave={handleUpdate}
+                    editable={editableItem && !updatingUser}
+                    onChangeText={setValue}
+                />
             </View>
-        </View>
+            {!updatingUser && !editableItem && <TouchableOpacity
+                hitSlop={20}
+                onPress={() => setEditableItem(true)}
+            >
+                <Feather name="edit-3" size={20} color="#0066e1" />
+            </TouchableOpacity>}
+
+            {!updatingUser && editableItem && <TouchableOpacity
+                hitSlop={20}
+                onPress={handleUpdate}
+            >
+                <Feather name="check" size={20} color="#0066e1" />
+            </TouchableOpacity>}
+            {updatingUser && <ActivityIndicator
+                size={20}
+                color="#0066e1"
+            />}
+        </>
     )
 }
