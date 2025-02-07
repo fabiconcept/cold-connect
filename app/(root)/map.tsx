6e165981213f7d01f58ce7503dcb2f1ba9e669e2
fabiconcept <favourajokubi@gmail.com>
2025-02-07 20:49:06 +0100

    Implement useStorageStore in map to display all storages and attempt to draw map direction to selected hub

--- app/(root)/map.tsx
+++ app/(root)/map.tsx
@@ -1,18 +1,34 @@
 import Header from '@/components/Map/Header';
 import { calculateRegion } from '@/lib/Map Utilities';
 import { useLocationStore, useStorageStore } from '@/store';
-import { View, Image } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+import { Text, View } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 export default function Map() {
-    const { latitude, longitude } = useLocationStore();
-
+    const { latitude, longitude, hasLocationPermission } = useLocationStore();
+    const { name } = useLocalSearchParams();
     const { storages } = useStorageStore();
+    const destination = storages.find((storage) => storage.name.trim().toLowerCase() === String(name).trim().toLowerCase());
+
+
+    if (!hasLocationPermission) return (
+        <View className='flex-1 items-center justify-center'>
+            <Text className='text-center text-2xl font-MontserratSemiBold text-red-500'>Storage Not Found</Text>
+        </View>
+    );
 
     const region = calculateRegion({
         latitude: latitude,
         longitude: longitude,
     });
+
+    if (!destination) return (
+        <View className='flex-1 items-center justify-center'>
+            <Text className='text-center text-2xl font-MontserratSemiBold text-red-500'>Storage Not Found</Text>
+        </View>
+    );
 
     return (
         <View className='flex-1 h-full w-full justify-center items-center'>
@@ -23,12 +39,11 @@ export default function Map() {
                 tintColor='black'
                 region={region}
                 mapType='standard'
-                showsMyLocationButton={false}
-                showsPointsOfInterest={false}
+                showsMyLocationButton={true}
                 showsUserLocation={true}
                 userInterfaceStyle='light'
             >
-                {storages && storages.map((storage) => {
+                {storages.length > 0 && storages.map((storage) => {
                     if (!storage.location) return null;
                     return (<Marker
                         key={storage.name}
@@ -37,14 +52,23 @@ export default function Map() {
                             latitude: Number(storage.location.latitude)
                         }}
                         title={storage.name}
-                    >
-                        <Image
-                            source={require("@/assets/images/cold/storage.png")}
-                            resizeMode='contain'
-                            className='w-5 h-5'
-                        />
-                    </Marker>)
+                        image={require("@/assets/images/cold/storage.png")}
+                    />)
                 })}
+
+                <MapViewDirections
+                    origin={{
+                        latitude: latitude,
+                        longitude: longitude
+                    }}
+                    destination={{
+                        latitude: destination.location!.latitude,
+                        longitude: destination.location!.longitude
+                    }}
+                    apikey={process.env.EXPO_PUBLIC_GOOGLE_API_KEY!}
+                    strokeWidth={5}
+                    strokeColor='#0286ff'
+                />
             </MapView>
             <Header />
         </View>
